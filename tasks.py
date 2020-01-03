7857e4011f65e7fa6a11dd0793cd78e9c187c3f0
chuckpr <cpeperanney@agbiome.com>
2020-01-03 10:16:25 -0500

    get theme from settings

--- tasks.py
+++ tasks.py
@@ -33,27 +33,14 @@ def clean(c):
         os.makedirs(CONFIG['deploy_path'])
 
 @task
-def build(c, theme_path=''):
+def build(c):
     """Build local version of site"""
-    if theme_path:
-        CONFIG.update({'theme_path': theme_path})
-        c.run('pelican -t {theme_path} -s {settings_base}'.format(**CONFIG))
-    else:
-        c.run('pelican -s {settings_base}'.format(**CONFIG))
+    c.run('pelican --debug -s {settings_base}'.format(**CONFIG))
 
 @task
-def rebuild(c, theme_path=''):
+def rebuild(c):
     """`build` with the delete switch"""
-    if theme_path:
-        CONFIG.update({'theme_path': theme_path})
-        c.run('pelican -d -t {theme_path} -s {settings_base}'.format(**CONFIG))
-    else:
-        c.run('pelican -d -s {settings_base}'.format(**CONFIG))
-
-# @task
-# def regenerate(c):
-#     """Automatically regenerate site upon file modification"""
-#     c.run('pelican -r -s {settings_base}'.format(**CONFIG))
+    c.run('pelican -d -s {settings_base}'.format(**CONFIG))
 
 @task
 def serve(c):
@@ -82,34 +69,41 @@ def preview(c):
     c.run('pelican -s {settings_publish}'.format(**CONFIG))
 
 @task
-def livereload(c, theme_path=''):
+def livereload(c):
     """Automatically reload browser tab upon file modification."""
     from livereload import Server
-    build(c, theme_path=theme_path)
+    build(c)
     server = Server()
+
     # Watch the base settings file
     server.watch(CONFIG['settings_base'],
-                 lambda: build(c, theme_path=theme_path))
+                 lambda: build(c))
+
     # Watch content source files
     content_file_extensions = ['.md', '.rst', '.ipynb']
     for extension in content_file_extensions:
         content_blob = '{0}/**/*{1}'.format(SETTINGS['PATH'], extension)
-        server.watch(content_blob, lambda: build(c, theme_path=theme_path))
+        server.watch(content_blob, lambda: build(c))
+
     # Watch the theme's templates and static assets
-    if theme_path:
+    if SETTINGS.get('THEME', None) is not None:
+
+        theme_path = SETTINGS['THEME']
+
         server.watch('{}/templates/*.html'.format(theme_path),
-                     lambda: build(c, theme_path=theme_path))
+                     lambda: build(c))
+
+        server.watch('{}/templates/**/*.html'.format(theme_path),
+                     lambda: build(c))
+
         static_file_extensions = ['.css', '.js', '.svg']
         for extension in static_file_extensions:
             static_file = '{0}/static/**/*{1}'.format(theme_path, extension)
-            server.watch(static_file, lambda: build(c, theme_path=theme_path))
+            server.watch(static_file, lambda: build(c))
+
     # Serve output path on configured port
     server.serve(port=CONFIG['port'], root=CONFIG['deploy_path'])
 
 @task
-def publish(c, theme_path=''):
-    if theme_path:
-        CONFIG.update({'theme_path': theme_path})
-        c.run('pelican -t {theme_path} -s {settings_publish}'.format(**CONFIG))
-    else:
-        c.run('pelican -s {settings_publish}'.format(**CONFIG))
+def publish(c):
+    c.run('pelican -d -s {settings_publish}'.format(**CONFIG))
