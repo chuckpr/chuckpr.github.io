43803d336cd2c0a2c21ae51478637fafba155504
chuckpr <cpeperanney@agbiome.com>
2020-05-17 17:57:09 -0400

    update configuration and bump pelican-jupyter-reader

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,16 +3,16 @@
 from __future__ import unicode_literals
 import os
 
-AUTHOR = 'Chuck Pepe-Ranney'
-SITENAME = 'A microbiologist with a data science problem'
-SITEURL = ''
-DESCRIPTION = '''A microbiology-themed technical blog for data scientists.'''
+AUTHOR = "Chuck Pepe-Ranney"
+SITENAME = "A microbiologist with a data science problem"
+SITEURL = ""
+DESCRIPTION = """A microbiology-themed technical blog for data scientists."""
 
-PATH = 'content'
+PATH = "content"
 
-TIMEZONE = 'US/Eastern'
+TIMEZONE = "US/Eastern"
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = "en"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -22,44 +22,49 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+    ("Pelican", "http://getpelican.com/"),
+    ("Python.org", "http://python.org/"),
+    ("Jinja2", "http://jinja.pocoo.org/"),
+    ("You can modify those links in your config file", "#"),
+)
 
 # Social widget
-SOCIAL = (('twitter', 'https://twitter.com/chuck_pr'),
-          ('github', 'https://github.com/chuckpr'),
-          ('linkedin', 'https://www.linkedin.com/in/chuckpr/'))
+SOCIAL = (
+    ("twitter", "https://twitter.com/chuck_pr"),
+    ("github", "https://github.com/chuckpr"),
+    ("linkedin", "https://www.linkedin.com/in/chuckpr/"),
+)
 
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
 
-ARTICLE_URL = 'posts/{slug}/'
-ARTICLE_SAVE_AS = 'posts/{slug}/index.html'
+ARTICLE_URL = "posts/{slug}/"
+ARTICLE_SAVE_AS = "posts/{slug}/index.html"
 
-TAG_URL = 'tag/{slug}/'
-TAG_SAVE_AS = 'tag/{slug}/index.html'
+TAG_URL = "tag/{slug}/"
+TAG_SAVE_AS = "tag/{slug}/index.html"
 
-PAGE_URL = 'pages/{slug}/'
-PAGE_SAVE_AS = 'pages/{slug}/index.html'
+PAGE_URL = "pages/{slug}/"
+PAGE_SAVE_AS = "pages/{slug}/index.html"
 
-TWITTER_USERNAME = 'chuckpr'
+TWITTER_USERNAME = "chuckpr"
 
-IGNORE_FILES = ['.ipynb_checkpoints*']
+IGNORE_FILES = [".ipynb_checkpoints*"]
 SUMMARY_MAX_LENGTH = 15
-STATIC_PATHS = ['images', 'notebooks/images', 'extra']
-EXTRA_PATH_METADATA = {
-    'images/favicon.ico': {'path': 'favicon.ico'}
-}
+STATIC_PATHS = ["images", "notebooks/images", "extra"]
+EXTRA_PATH_METADATA = {"images/favicon.ico": {"path": "favicon.ico"}}
 
-if os.environ.get('PELICAN_THEME', None) is not None:
-    THEME = os.environ['PELICAN_THEME']
+if os.environ.get("PELICAN_THEME", None) is not None:
+    THEME = os.environ["PELICAN_THEME"]
 
 from pelican.plugins import pelican_jupyter_reader
-PLUGINS = [pelican_jupyter_reader,]
+
+PLUGINS = [
+    pelican_jupyter_reader,
+]
 
 # # Uncomment for plugin development
 # if os.environ.get('PELICAN_PLUGIN_PATHS', None) is not None:
@@ -67,26 +72,26 @@ PLUGINS = [pelican_jupyter_reader,]
 #     PLUGINS = ['pelican_jupyter_reader',]
 
 import extended_sitemap
+
 PLUGINS.append(extended_sitemap)
 
-TOGGLE_ALL_TAG = 'toggle_all'
-TOGGLE_CODE_TAG = 'toggle_code'
-TOGGLE_CODE_BUTTON_TEXT = 'toggle code'
-TOGGLE_ALL_BUTTON_TEXT = 'show me'
-HIDE_CELL_TAG = 'disappear'
+TOGGLE_ALL_TAG = "toggle_all"
+TOGGLE_CODE_TAG = "toggle_code"
+TOGGLE_CODE_BUTTON_TEXT = "toggle code"
+TOGGLE_ALL_BUTTON_TEXT = "show me"
+HIDE_CELL_TAG = "disappear"
 
 
 # Theme settings
 NAV_LINKS = [
-    ('about', 'pages/about/'),
+    ("about", "pages/about/"),
 ]
-EXTRA_CSS = dict(
-    article="extra/css/notebook.css"
-)
+EXTRA_CSS = dict(article="extra/css/notebook.css")
 
 
 # Instantiate a config object and call it NBCONVERT_CONFIG
 from traitlets.config import Config
+
 NBCONVERT_CONFIG = Config()
 
 # This removes Jupyter input prompts
@@ -95,18 +100,19 @@ NBCONVERT_CONFIG.HTMLExporter.exclude_input_prompt = True
 # This makes Jupyter magic cells have proper code highlighting
 # regardless of the language of the notebook kernel
 NBCONVERT_CONFIG.HighlightMagicsPreprocessor.enabled = True
-NBCONVERT_CONFIG.HighlightMagicsPreprocessor.languages.update({"%%svg": "xml",
-                                                               "%%html": "html"})
+NBCONVERT_CONFIG.HighlightMagicsPreprocessor.languages.update(
+    {"%%svg": "xml", "%%html": "html"}
+)
 
 # select the nbconvert template
-NBCONVERT_CONFIG.HTMLExporter.template_path.append('.')
-NBCONVERT_CONFIG.HTMLExporter.template_file = 'blog'
+NBCONVERT_CONFIG.HTMLExporter.template_path.append(".")
+NBCONVERT_CONFIG.HTMLExporter.template_file = "blog"
 
 # extract images to files
 NBCONVERT_CONFIG.ExtractOutputPreprocessor.enabled = True
-NBCONVERT_CONFIG.ExtractOutputPreprocessor.output_filename_template = \
-    'images/{unique_key}_{cell_index}_{index}{extension}'
+NBCONVERT_CONFIG.ExtractOutputPreprocessor.output_filename_template = (
+    "images/{unique_key}_{cell_index}_{index}{extension}"
+)
 
 # adujst the anchor links
-# NBCONVERT_CONFIG.HTMLExporter.anchor_link_text = u' ¶'
-NBCONVERT_CONFIG.HTMLExporter.anchor_link_text = u' ⇦'
+NBCONVERT_CONFIG.HTMLExporter.anchor_link_text = " ⇦"
